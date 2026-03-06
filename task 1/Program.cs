cd48f02983ecf1d6f2f14ccb09adc3ac2c330c55
unknown <Classtime@PC_6_213_2.aztu.edu.az>
2026-03-06 12:08:08 +0400

    mesaj

---
+++ task 1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Concurrent;
+using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+int x = 35;
+int S = 1;
+for (int i = 0; i <= x; i++)
+{
+    S = S + i;
+}
+Console.WriteLine(S);
