49929cd85a99ba3b457387324ff56bdbe274f24a
unknown <Classtime@PC_6_213_2.aztu.edu.az>
2026-03-06 12:27:35 +0400

    mesaj

--- task 1/Program.cs
+++ task 1/Program.cs
@@ -1,11 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Collections.Concurrent;
-using System.ComponentModel.Design;
-using System.Runtime.CompilerServices;
-int x = 35;
-int S = 1;
-for (int i = 0; i <= x; i++)
+﻿
+
+Console.WriteLine("bir eded daxil edin");
+int eded = Convert.ToInt32(Console.ReadLine());
+if (eded > 0)
 {
-    S = S + i;
+    Console.WriteLine("eded musbetdir");
 }
-Console.WriteLine(S);
+else if (eded < 0)
+    Console.WriteLine("Cavab menfidir");
+else
+    Console.WriteLine("cavab alinmir");
