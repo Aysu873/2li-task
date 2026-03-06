4939085d5a0c1a4f49deb34214dd177322c2db6d
Classtime <Classtime@PC_6_213_3.aztu.edu.az>
2026-03-06 13:28:48 +0400

    second

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ededi daxil edin: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int kub = num * num * num;
+
+Console.WriteLine("Ededin kubu: " + kub);
+Console.WriteLine("Ededin kubu: " + kub);
